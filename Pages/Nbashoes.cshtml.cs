b78f4db0daee1e178ac498cb01063045788cc68e
Serna0421 <54141647+Serna0421@users.noreply.github.com>
2023-03-17 22:58:51 +0800

    Add project files.

---
+++ Pages/Nbashoes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhotoAlbum_Serna.Pages
+{
+    public class NbashoesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
